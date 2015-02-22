4868ebf529f5771baeb69bb594dc4697ba10aaba
megilson <megilson@gmail.com>
2015-02-21 21:00:32 -0600

    Updates

--- resume/js/grayscale.js
+++ resume/js/grayscale.js
@@ -3,37 +3,38 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
- (function() {
-    var v=window,d=document;
-    v.onresize = function() {
-        var w = v.innerWidth ? v.innerWidth :
-                d.documentElement.clientWidth,
-            h = v.innerHeight ? v.innerHeight : 
-                d.documentElement.clientHeight,
-            s = d.getElementById('WSzPlgIn'),
-            ss;
-        if (!s) {
-            s = d.createElement('div');
-            s.id = 'WSzPlgIn';
-            d.body.appendChild(s);
-            s.onclick = function() {
-                s.parentNode.removeChild(s)
-            };
-            ss = s.style;
-            ss.position = 'absolute';
-            ss.bottom = 0;
-            ss.right = 0;
-            ss.backgroundColor = 'black';
-            ss.opacity = '.5';
-            ss.color = 'white';
-            ss.fontFamily = 'monospace';
-            ss.fontSize = '10pt';
-            ss.padding = '5px';
-            ss.textAlign = 'right';
-        }
-        s.innerHTML = 'w ' + w + '<br />h ' + h;
-    };
-})()
+//Screen Size Function for testing responsive formats
+// (function() {
+//   var v=window,d=document;
+//    v.onresize = function() {
+//        var w = v.innerWidth ? v.innerWidth :
+//                d.documentElement.clientWidth,
+//            h = v.innerHeight ? v.innerHeight : 
+//                d.documentElement.clientHeight,
+//            s = d.getElementById('WSzPlgIn'),
+//            ss;
+//        if (!s) {
+//            s = d.createElement('div');
+//            s.id = 'WSzPlgIn';
+//            d.body.appendChild(s);
+//            s.onclick = function() {
+//                s.parentNode.removeChild(s)
+//            };
+//            ss = s.style;
+//            ss.position = 'absolute';
+//            ss.bottom = 0;
+//            ss.right = 0;
+//            ss.backgroundColor = 'black';
+//            ss.opacity = '.5';
+//            ss.color = 'white';
+//            ss.fontFamily = 'monospace';
+//            ss.fontSize = '10pt';
+//            ss.padding = '5px';
+//            ss.textAlign = 'right';
+//        }
+//        s.innerHTML = 'w ' + w + '<br />h ' + h;
+//    };
+//})()
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
